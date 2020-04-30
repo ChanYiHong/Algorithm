8b8b05f0f032c607e96ce03691bfba1b1e428eed
ChanYi <chanyinara@naver.com>
2020-04-30 23:16:09 +0900

    1012번

---
+++ 그래프/[그래프] 유기농 배추 복사본.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <utility>
+#include <cstring>
+using namespace std;
+int dx[4] = {1,-1,0,0};
+int dy[4] = {0,0,1,-1};
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    int T;
+    cin >> T;
+    for(int t = 0; t < T; t++){
+        int arr[51][51];
+        bool check[51][51];
+        int n, m, k;
+        cin >> n >> m >> k;
+        memset(arr, 0, sizeof(arr));
+        memset(check, false, sizeof(check));
+        for(int s = 0; s < k; s++){
+            int x, y;
+            cin >> x >> y;
+            arr[y][x] = 1;
+        }
+        int answer = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(arr[i][j] == 1 && !check[i][j]){
+                    queue<pair<int, int> > q;
+                    q.push(make_pair(i,j));
+                    check[i][j] = true;
+                    while(!q.empty()){
+                        int a = q.front().first;
+                        int b = q.front().second;
+                        q.pop();
+                        for(int z = 0; z < 4; z++){
+                            int nx = a + dx[z];
+                            int ny = b + dy[z];
+                            if(nx >= 0 && nx < m && ny >= 0 && ny < n){
+                                if(arr[nx][ny] == 1 && !check[nx][ny]){
+                                    check[nx][ny] = true;
+                                    q.push(make_pair(nx,ny));
+                                }
+                            }
+                        }
+                    }
+                    answer++;
+                }
+            }
+        }
+        cout << answer << '\n';
+    }
+    return 0;
+}
