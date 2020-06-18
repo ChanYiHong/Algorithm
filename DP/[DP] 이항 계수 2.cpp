aeb512ed5cdfd2ee78486c931344d4f9f33f0a0c
ChanYiHong <chanyinara@naver.com>
2020-06-18 23:59:35 +0900

    11051번 수정

--- DP/[DP] 이항 계수 2.cpp
+++ DP/[DP] 이항 계수 2.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 #include <algorithm>
+#include <cstring>
+#define M 10007
 using namespace std;
+int D[1001][1001];
+
+int dp(int N, int K){
+    
+    if(K == 1 || N == 1) return 1;
+    if(K == 0) return 0;  
+    int& ret = D[N][K];
+    if(ret != -1) return ret;
+    
+    ret = dp(N-1, K) + dp(N-1, K-1);
+    return ret%M;
+}
 
 int main()
 {
     ios_base::sync_with_stdio(false); cin.tie(nullptr);
-
-    for(int i = 0; i < 5; ++i){
-        cout << i << '\n';
-    }
-
-    for(int i = 0; i < 5; i++){
-        cout << i << '\n';
-    }
-
-    string name = "";
-    name = name + "hello";
-    cout << name;
-
-    int k = 1;
-    string temp = "";
-    temp += (k + '0');
-    cout << temp;
-
+    int n, k;
+    cin >> n >> k;
+    memset(D, -1, sizeof(D));
+    cout << dp(n, k);
+    
     return 0;
 }
