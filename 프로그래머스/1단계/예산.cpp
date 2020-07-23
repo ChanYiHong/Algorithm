7527fc44970873aa14c4d984dd1fcbabc34b75fa
ChanYiHong <chanyinara@naver.com>
2020-07-23 17:45:18 +0900

    프로그래머스 1단계

---
+++ 프로그래머스/1단계/예산.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    
+    sort(d.begin(), d.end());
+    for(int money : d){
+        if(budget - money >= 0){
+            answer++;
+            budget -= money;
+        }
+    }
+    
+    return answer;
+}
