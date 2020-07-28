7a24a064b7cb6981b2c67e603e0ecfc666c44a72
ChanYiHong <chanyinara@naver.com>
2020-07-28 11:30:56 +0900

    프로그래머스 1단계

---
+++ 프로그래머스/1단계/자릿수 더하기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int solution(int n)
+{
+    int answer = 0;
+    for(int i = n; i > 0; i/=10){
+        answer += i%10;
+    }
+    
+    return answer;
+}
