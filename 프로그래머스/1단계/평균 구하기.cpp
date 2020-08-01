2e9a985a11c075f5e8a31ee1cc839cd217e8f23a
ChanYiHong <chanyinara@naver.com>
2020-08-01 12:34:11 +0900

    프로그래머스 1단계 accumulate 배열의 합

--- 프로그래머스/1단계/평균 구하기.cpp
+++ 프로그래머스/1단계/평균 구하기.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 double solution(vector<int> arr) {
     double answer = 0;
-    answer = accumulate(arr.begin(), arr.end(), 0);
+    //answer = accumulate(arr.begin(), arr.end(), 0);
     answer /= arr.size();
     return answer;
 }
