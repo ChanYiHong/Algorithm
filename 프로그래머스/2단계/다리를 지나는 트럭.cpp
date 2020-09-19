e6a7a171ad1322afa6e67bd6c001851770b3110f
ChanYiHong <chanyinara@naver.com>
2020-09-20 01:30:57 +0900

    프로그래머스 2단계 스택/큐

--- 프로그래머스/2단계/다리를 지나는 트럭.cpp
+++ 프로그래머스/2단계/다리를 지나는 트럭.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <iostream>
 
 using namespace std;
 
@@ -18,18 +19,19 @@ int solution(int bridge_length, int weight, vector<int> truck_weights) {
         
         time++;
         
-        if(time - q.front().second == bridge_length){
+        if(time - q.front().second >= bridge_length){
             currentWeight -= q.front().first;
             q.pop();
         }
         
-        if(index == truck_weights.size()) break;
-        
-        if(currentWeight + truck_weights[index] < weight){
-            q.push({truck_weights[index], time});
-            currentWeight += q.front().first;
-            index++;
-        }
+        if(index < truck_weights.size()){
+
+            if(currentWeight + truck_weights[index] <= weight){
+                q.push({truck_weights[index], time});
+                currentWeight += truck_weights[index];
+                index++;
+            }
+        } 
         
     }
     
