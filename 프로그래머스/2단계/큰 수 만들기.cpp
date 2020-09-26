6625c5b318aa2e052215a18a5b6fe2517950c5c3
ChanYiHong <chanyinara@naver.com>
2020-09-27 01:02:45 +0900

    프로그래머스 2단계 그리디.. 겨우 풀었다

--- 프로그래머스/2단계/큰 수 만들기.cpp
+++ 프로그래머스/2단계/큰 수 만들기.cpp
@@ -8,27 +8,34 @@ using namespace std;
 string solution(string number, int k) {
     string answer = "";
     
-    int len = number.length() - k;
+    int len = number.length();
+    int req = len - k;
     int start = 0;
     int index = 0;
+    int current_answer_length = 0;
     
-    for(int i = len; i > 0; i--){
-        int maxnum = 0;
-        for(int j = start; j < number.length() - i + 1; j++){
+    while(k > 0){
+        int maxnum = -1;
+        int end = len - (req - current_answer_length - 1);
+        for(int j = start; j < end; j++){
             int num = number[j] - '0';
-            
             if(maxnum < num){
                 index = j;
                 maxnum = num;
             }
         }    
         answer += number[index];
+        k = k - (index - start);
         start = index + 1;
-        if(start >= number.length() - i + 1) break;
+        index = start;
+        current_answer_length++;
+        if(current_answer_length == req) break;
     }
     
-    for(int i = start; i < number.length(); i++){
-        answer += number[i];
+    if(req != current_answer_length){
+        for(int i = start; i < number.length(); i++){
+            answer += number[i];
+        }
     }
     
     return answer;
