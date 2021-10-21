865f12adafc699eac7b5fedc215d4a5947a7a104
ChanYiHong <chanyinara@naver.com>
2021-10-21 15:21:18 +0900

    Olimplanet assignment

---
+++ JAVA/src/OlimPlanet/WhyHomeViewNeeded.java
@@ -0,0 +1,105 @@
+package OlimPlanet;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class WhyHomeViewNeeded {
+
+    static class Edge implements Comparable<Edge> {
+        private int node1;
+        private int node2;
+        private int weight;
+
+        public Edge(int node1, int node2, int weight) {
+            this.node1 = node1;
+            this.node2 = node2;
+            this.weight = weight;
+        }
+
+        public int getNode1() {
+            return node1;
+        }
+
+        public int getNode2() {
+            return node2;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return Integer.compare(this.weight, o.getWeight());
+        }
+    }
+
+    private static boolean isSameParent(int a, int b, int parent[]) {
+        return findParent(a, parent) == findParent(b, parent);
+    }
+
+    private static void union(int a, int b, int parent[]) {
+        int x = findParent(a, parent);
+        int y = findParent(b, parent);
+
+        if (x != y) {
+            if (x > y) {
+                parent[x] = y;
+            }
+            else {
+                parent[y] = x;
+            }
+        }
+    }
+
+    private static int findParent(int x, int parent[]) {
+        if (x != parent[x]) {
+            return parent[x] = findParent(parent[x], parent);
+        }
+        return parent[x];
+    }
+
+    public static void main(String[] args) {
+
+//        int[][] input = {{0, 611, 648}, {611, 0, 743}, {648, 743, 0}};
+        int[][] input = {{0, 326, 503, 290}, {326, 0, 225, 395}, {503, 225, 0, 620}, {290, 395, 620, 0}};
+
+        int len = input.length;
+        int[] parent = new int[len];
+        int answer = 0;
+
+        for (int i = 0; i < len; i++) {
+            parent[i] = i;
+        }
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len - i; j++) {
+                if (i == j) continue;
+
+                int node1 = i;
+                int node2 = j;
+                int weight = input[i][j];
+
+                pq.offer(new Edge(node1, node2, weight));
+
+            }
+        }
+
+        while(!pq.isEmpty()) {
+            Edge edge = pq.poll();
+
+            int node1 = edge.getNode1();
+            int node2 = edge.getNode2();
+            int weight = edge.weight;
+            if (!isSameParent(node1, node2, parent)) {
+                union(node1, node2, parent);
+                answer += weight;
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+}
